ae0e327e04de7b634dd02f2b69e56c3270ac6aa0
Alnaimi- <al.alnaimi@gmail.com>
2015-12-07 22:25:19 +0000

    Added additional views.

--- src/Views.hs
+++ src/Views.hs
@@ -14,21 +14,26 @@ import qualified Data.Text.Lazy.Encoding as TL
 import Control.Monad.IO.Class
 import Web.Scotty.Internal.Types
 
-articlesList :: [Article] -> ActionM ()
-articlesList articles = json articles
+{- Following is concerned with displaying the Tweet -}
+
+listedTweets :: [Tweet] -> ActionM ()
+listedTweets tweets = json tweets
 
 viewArticle :: Maybe Article -> ActionM ()
 viewArticle Nothing = json ()
 viewArticle (Just article) = json article
 
-createdArticle :: Maybe Article -> ActionM ()
-createdArticle article = json ()
-
-updatedArticle :: Maybe Article -> ActionM ()
-updatedArticle article = json ()
-
-deletedArticle :: TL.Text -> ActionM ()
-deletedArticle id = json ()
-
 insertedTweets :: Maybe [Tweet] -> ActionM ()
 insertedTweets tweets = json tweets
+
+deletedTweet :: TL.Text -> ActionM ()
+deletedTweet id = json ()
+
+{- Following is concerned with displaying the User -}
+
+updatedUser :: Maybe Article -> ActionM ()
+updatedUser article = json ()
+
+viewUser :: Maybe User -> ActionM ()
+viewUser Nothing = json ()
+viewUser (Just user) = json user
