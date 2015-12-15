51e5dae3c2b64c082ea8e07bd853fe90eae33035
Alnaimi- <al.alnaimi@gmail.com>
2015-12-15 16:59:44 +0000

    Added comments.

--- src/Exceptions.hs
+++ src/Exceptions.hs
@@ -4,6 +4,15 @@ import Network.HTTP.Conduit
 import Network.HTTP.Types.Status   (statusCode)
 import qualified Control.Exception as E
 
+-- Most error handling was done through Maybe
+-- As such type of handling allowed me to be
+-- flexible with the front-end displaying 
+-- errors which should be visible to the user
+--
+-- Exceptions are more for the back-end. 
+-- Handling errors that are and should be
+-- transparent to the end user.
+--
 -- | HTTP StatusCode Exception handler
 myHttpException :: HttpException -> IO (Maybe a)
 myHttpException (StatusCodeException s _ _) = 
